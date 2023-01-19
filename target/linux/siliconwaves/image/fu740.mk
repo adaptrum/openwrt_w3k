0a65133e4b40d5696d2e5aed5de1a09822a7c27f
Richard Dai <richarddai@163.com>
2023-01-19 17:56:28 +0800

    riscv: siliconwaves: fixed target/linux/siliconwaves/Makfile to simple
    Signed-off-by: Richard Dai <richarddai@163.com>

--- target/linux/siliconwaves/image/fu740.mk
+++
@@ -1,9 +0,0 @@
-define Device/Default
-  PROFILES := Default
-  FILESYSTEMS := squashfs ext4
-  KERNEL_INSTALL := 1
-  KERNEL_NAME := Image
-  KERNEL := kernel-bin
-  KERNEL_LOADADDR := 0x80020000
-  IMAGES :=
-endef
